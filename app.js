c685edc69a6bf20825ffdb0075812ac470bffb53
Ananmay 125 <ananmay125@gmail.com>
2024-03-28 09:31:16 +0530

    login change

--- app.js
+++ app.js
@@ -1,62 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+
 const app = express();
-const PORT = 3000;
-const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
-const {connect, Schema, model} = require("mongoose");
-
-connect('mongodb+srv://Ananmay125:pma4got0f9XBkPof@darkweb.9zwo3qf.mongodb.net/?retryWrites=true&w=majority&appName=DarkWeb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
-
-const userSchema = new Schema({
-    username: { type: String, unique: true },
-    password: String
-});
-
-const User = model('User', userSchema);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/register.html');
-});
-
-app.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const newUser = new User({
-            username: req.body.username,
-            password: hashedPassword
-        });
-        await newUser.save();
-        res.redirect('/login');
-    } catch {
-        res.redirect('/');
-    }
-});
-
-app.post('/login', async (req, res) => {
-    const user = await User.findOne({ username: req.body.username });
-    if (user) {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.sendFile(__dirname + '/index.html');
-        } else {
-            res.sendFile(__dirname + '/index.html');
-        }
-    } else {
-        res.sendFile(__dirname + '/index.html');
-    }
-});
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(session({ secret: 'your-secret', resave: true, saveUninitialized: true }));
+
+mongoose.connect('mongodb+srv://Ananmay125:pma4got0f9XBkPof@darkweb.9zwo3qf.mongodb.net/?retryWrites=true&w=majority&appName=DarkWeb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const User = mongoose.model('User', new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -90,10 +49,48 @@ app.get('/spiderman.html', (req, res) => {
     res.sendFile(__dirname + '/spiderman.html');
 })
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running,and App is listening on port "+ PORT)
-    else
-        console.log("Error occurred, server can't start", error);
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await User.create({ username, password: hashedPassword });
+        req.session.userId = user._id;
+        res.redirect('/login');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error registering user');
     }
-);
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        req.session.userId = user._id;
+        res.redirect('/index');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/index', (req, res) => {
+    if (!req.session.userId) {
+        return res.redirect('/login');
+    }
+    res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
